7a5beefdfd1ea02d0a7755f138948991a3e68a34
steve <1606880712@qq.com>
2022-08-15 17:05:54 +0800

    props补充

--- vue/20_vue项目/20_03_prop属性以及用法/vue-learn/src/components/Student.vue
+++ vue/20_vue项目/20_03_prop属性以及用法/vue-learn/src/components/Student.vue
@@ -5,8 +5,9 @@
         <h2>{{studentAge}}</h2>
         <h2>{{time}}</h2>
         <h2>{{who}}</h2>
-        <h2>{{age+1}}</h2>
+        <h2>{{ownAge}}</h2>
         <button @click="showName">点我提示学生名</button>
+        <button @click="updateAge">修改传入的年龄</button>
     </div>
 </template>
 
@@ -17,15 +18,48 @@ const student = ({
     data(){
         return{
             studentName:'张三',
-            studentAge:'19'
+            studentAge:'19',
+            ownAge:this.age
         }
     },
     methods: {
         showName(){
             alert(this.studentName);
+        },
+        updateAge(){
+            this.ownAge++
         }
     },
-    props:['time','who','age']
+    //简单声明接收
+    // props:['time','who','age']
+
+    //接收的同时对数据进行类型限制
+    // props:{
+    //     time:String,
+    //     who:String,
+    //     age:Number,
+    // }
+
+    //接收的同时对数据进行类型限制+默认值的指定+必要性的限制
+    //props优先级比data要高,该组件初始化时优先准备props,再配置data(){}中数据
+    props:{
+        time:{
+            type:String,    // time类型是字符串
+            required:true, // time是必要的
+            default:''
+        },
+        age:{
+            type:Number,    // age类型是数字
+            required:true, // age是必要的
+            default:0
+        },
+        who:{
+            type:String,    // who类型是字符串
+            required:true, // who是必要的
+            default:''
+        },
+    }
+
 })
 export default student
 </script>
