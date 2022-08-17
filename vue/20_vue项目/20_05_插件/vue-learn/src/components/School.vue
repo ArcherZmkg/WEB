a6f9a14ae85760e5ab280aa1bc8815efdbc5f95e
steve <1606880712@qq.com>
2022-08-17 16:31:31 +0800

    插件相关

---
+++ vue/20_vue项目/20_05_插件/vue-learn/src/components/School.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- 组件结构 -->
+    <div class="demo">
+        <h2>{{age}}</h2>
+        <h2>{{name}}</h2>
+    </div>
+</template>
+
+<script>
+//组件交互相关代码(数据、方法等等)
+const School = ({
+    name:'School',
+    data(){
+        return{
+            age:18,
+            name:'张三'
+        }
+    },
+   
+
+})
+export default School
+</script>
+
+<style>
+/*组件的样式*/
+.demo{
+    background-color: aquamarine;
+}
+</style>
---
+++ vue/20_vue项目/20_05_插件/vue-learn/src/plugins.js
@@ -0,0 +1,7 @@
+const plugin =  {
+    install(Vue){
+        console.log('@@install',Vue);
+    }
+}
+
+export default plugin
