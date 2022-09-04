299356c57cf90364e03e75370e3b42d817872fbd
steve <1606880712@qq.com>
2022-09-04 17:42:47 +0800

    ref函数的使用

--- vue3/vue-cli构建/vue3-test/README.md
+++ vue3/vue-cli构建/vue3-test/README.md
@@ -22,3 +22,15 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+##  ref函数
+作用:定义一个响应式的数据
+语法:const xxx = ref(initValue)
+    创建一个包含响应式数据的引用对象(reference对象)
+    JS中操作数据:xxx.value
+    模板中读取数据:不需要value,直接<div>{{xxx}}</div>
+备注:
+    接收的数据可以是:基本类型,也可以是对象类型
+    基本类型的数据:响应式依然是依靠Object.defineProperty()的get和set完成的
+    对象类型的数据:内部求助了Vue3.0中的一个新函数-----reactive函数
--- vue3/vue-cli构建/vue3-test/src/App.vue
+++ vue3/vue-cli构建/vue3-test/src/App.vue
@@ -1,15 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h3>个人信息</h3>
+      <hr>
+      <h3>姓名:{{name}}</h3>
+      <h3>年龄:{{age}}</h3>
+      <h3>工作种类:{{job.type}}</h3>
+      <h3>工作薪资:{{job.salary}}</h3>
+      <button @click="changeInfo">点击修改信息</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from 'vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    // ref处理基本数据类型确实使用的是数据劫持方式即get和set,但如果处理的是引用数据类型诸如对象之类的则是运用的es6中的proxy代理
+
+    let name = ref('张三');
+    let age = ref(19);
+    let job = ref({
+      type:'node',
+      salary:'33k'
+    })
+
+
+    // 修改信息方法
+    const changeInfo=()=>{
+      name.value = '李四';
+      age.value = 20;
+      job.value.type = 'HAHA';
+      job.value.salary = '10k'
+    }
+
+    //返回一个对象(常用)
+    return {
+      name,
+      age,
+      job,
+      changeInfo
+    }
+
+    //返回一个函数(渲染函数)
+    //return ()=> h('h1','尚硅谷')
   }
 }
 </script>
