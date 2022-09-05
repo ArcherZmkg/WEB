668cde3c765ab73ec3ecf2fd0a78f6cd94281f82
steve <1606880712@qq.com>
2022-09-05 17:03:30 +0800

    setup相关属性

---
+++ vue3/05_setup的两个注意点/setup-learning/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Home :person="person" @hello="show">
+      <template v-slot:qwe>
+        <div>
+          <span>???</span>
+        </div>
+      </template>
+    </Home>
+  </div>
+  
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  setup(){
+    let person = {
+      age:18,
+      name:'张三'
+    }
+
+    const show=(value)=>{
+      alert(`你好啊,你触发了,我收到的emit值为${value}`)
+    }
+
+    return {
+      person,
+      show
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3/05_setup的两个注意点/setup-learning/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>准备一个人的信息</h1>
+    <h2>姓名：{{person.name}}</h2>
+    <h2>年龄：{{person.age}}</h2>
+    <slot name="qwe"></slot>
+    <button @click="hello">触发hello</button>
+</template>
+
+<script>
+import {reactive} from 'vue'
+export default {
+    name:'Home',
+    props:['person'],
+    emits:['hello'],
+    // beforeCreate(){
+    //     console.log('--------beforeCreated');
+    // },
+    setup(props,context){
+        // console.log('--------setup',props);
+        // console.log('--------setup',context);
+        // console.log('--------setup',context.attrs); //  详单与Vue2中的$attrs
+        // console.log('--------setup',context.emit);  //  触发自定义事件的
+        console.log('--------setup',context.slots);  //插槽
+        //方法
+        function hello(){
+            context.emit("hello",'6666');
+        }
+        return{
+            hello
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
