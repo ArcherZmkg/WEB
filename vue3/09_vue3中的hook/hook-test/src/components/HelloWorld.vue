b32b0c6464426781e1d694ebb67a096627c881c3
steve <1606880712@qq.com>
2022-09-07 18:12:54 +0800

    hooks相关

---
+++ vue3/09_vue3中的hook/hook-test/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>当前点击鼠标的坐标为:x:{{point.x}},y:{{point.y}}</h2>
+  </div>
+</template>
+
+<script>
+import usePoint from '../hooks/usePoint'
+export default {
+  name:'HelloWorld',
+  setup(){
+    const point = usePoint();
+    return{
+      point
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
