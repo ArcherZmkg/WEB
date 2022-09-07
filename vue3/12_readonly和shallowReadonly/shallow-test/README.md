5e061365a1ac26710a034752cfa7fe223a925b9d
steve <1606880712@qq.com>
2022-09-07 19:12:13 +0800

    readonly和shallowReadonly

---
+++ vue3/12_readonly和shallowReadonly/shallow-test/README.md
@@ -0,0 +1,29 @@
+# hook-test
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## readonly 与 shallowReadonly
+    readonly:让一个响应式数据变为只读的(深只读)。
+    shallowReadonly:让一个响应式数据变为只读的(浅只读)。
+    应用场景:不希望数据被修改时。
---
+++ vue3/12_readonly和shallowReadonly/shallow-test/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>当前sum：{{sum}}</h2>
+    <button @click="sum++">sum加</button>
+    <hr>
+    <h3>姓名：{{name}}</h3>
+    <hr>
+    <h3>年龄：{{age}}</h3>
+    <h3>工作薪资：{{job.cash}}K</h3>
+    <h3>工作时长：{{job.time}}</h3>
+    <button @click="name+='!'">操作姓名</button>
+    <button @click="age++">增龄</button>
+    <button @click="job.cash++">涨薪</button>
+  </div>
+</template>
+
+<script>
+import { reactive,ref, toRefs, readonly, shallowReadonly} from '@vue/reactivity'
+export default {
+  name:'HelloWorld',
+  setup(){
+    let person = reactive({
+      name:'张三',
+      age:18,
+      job:{
+        cash:30,
+        time:'8h'
+      }
+    });
+    let sum = ref(0);
+
+    person = shallowReadonly(person);
+    // person = readonly(person);
+    // sum = readonly(sum);
+
+
+    
+    return{
+      sum,
+      ...toRefs(person)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
