f2e58cf7cf3cbb3a8e200cfb1b56a556e7abc563
steve <1606880712@qq.com>
2022-09-09 16:55:34 +0800

    suspense异步引入组件用法

---
+++ vue3/19_Suspense/Suspense-test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app">
+    <h3>我是App组件</h3>
+    <Suspense>
+      <!-- default插槽里放入正常加载显示的组件 -->
+      <template v-slot:default>
+        <Son></Son>
+      </template>
+      <!-- fallback插槽里放入未能及时加载显示该组件的应对情况 -->
+      <template v-slot:fallback>
+        <h3>稍等,加载中......</h3>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script>
+  import {ref} from 'vue';
+  // import Son from './components/son.vue';
+  import {defineAsyncComponent} from "vue"; //静态引入
+  const Son = defineAsyncComponent(()=>import('./components/son.vue'));//异步引入
+  export default {
+    name: 'App',
+    components:{
+      Son
+    },
+    setup(){
+     
+    }
+  }
+</script>
+
+<style>
+  .app{
+    background: gray;
+    padding: 10px;
+  }
+</style>
---
+++ vue3/19_Suspense/Suspense-test/src/components/son.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <h3>我是子组件(子)</h3>
+    {{sum}}
+  </div>
+</template>
+
+<script>
+  import {ref} from 'vue';
+  export default {
+      name:'Son',
+      async setup(){
+        let sum = ref(0);
+        let p = new Promise((reslove,reject)=>{
+          setTimeout(()=>{
+            reslove({sum})
+          },3000)
+        })
+        return await p
+      }
+  }
+</script>
+
+<style scoped>
+  .main{
+    background-color: yellow;
+  }
+</style>
